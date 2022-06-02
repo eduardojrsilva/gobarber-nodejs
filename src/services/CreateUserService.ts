ab9759df4bcd1e8bec77f28905d85d563ab9e97b
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-06-02 12:04:06 -0300

    user registration

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,48 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({
+    name,
+    email,
+    password,
+  }: Request): Promise<Omit<User, 'password'>> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Email address already used');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+  }
+}
+
+export default CreateUserService;
