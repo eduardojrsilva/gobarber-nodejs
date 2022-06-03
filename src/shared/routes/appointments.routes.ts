8eecb6212e4af924a69d154e5caeedb8a1772fca
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-06-03 08:55:00 -0300

    separating modules

--- src/shared/routes/appointments.routes.ts
+++ src/shared/routes/appointments.routes.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import CreateAppointmentService from '../services/CreateAppointmentService';
+import AppointmentsRepository from '../../modules/appointments/repositories/AppointmentsRepository';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import CreateAppointmentService from '../../modules/appointments/services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
