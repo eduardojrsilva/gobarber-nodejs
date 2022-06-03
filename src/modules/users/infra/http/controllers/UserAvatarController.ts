c5dbb8670be6d4a4d49b3c6dc9cb6079a6b0d018
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-06-03 13:28:18 -0300

    adding controllers

---
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+    if (!request.file) throw new Error('Is necessary to update a file');
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
+
+    return response.json(user);
+  }
+}
